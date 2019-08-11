62959cd6253d2cad56ca91cfd8c9f9cc77364e06
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2019-08-11 23:22:27 +0300

    Add a pattern for date

--- YourSpentTimeCounter.py
+++ YourSpentTimeCounter.py
@@ -37,7 +37,41 @@ while True:
         if not dirChecker:
             print("File not founded\n\n")
             continue
-        date = input("\n\nWhat date is it today?\n")
+        date = input("\n\nWhat date is it today? (dd/mm/yyyy)\n")
+        dateChecker = date.split("/")
+        if len(dateChecker) != 3:
+            print("Incorrect date's value\n\n")
+            continue
+        try:
+            int(dateChecker[0])
+        except ValueError:
+            print("Incorrect day's value\n\n")
+            continue
+        try:
+            int(dateChecker[1])
+        except ValueError:
+            print("Incorrect mounth's value\n\n")
+            continue
+        if int(dateChecker[1]) > 12 or int(dateChecker[1]) < 0:
+            print("Incorrect mounth's value\n\n")
+            continue
+        if int(dateChecker[1]) == 1 or int(dateChecker[1]) == 3 or int(dateChecker[1]) == 5 or int(dateChecker[1]) == 7 or int(dateChecker[1]) == 8 or int(dateChecker[1]) == 10 or int(dateChecker[1]) == 12:
+            if int(dateChecker[0]) > 31 or int(dateChecker[0]) < 0:
+                print("Incorrect day's value\n\n")
+                continue
+        if int(dateChecker[1]) == 11 or int(dateChecker[1]) == 4 or int(dateChecker[1]) == 6 or int(dateChecker[1]) == 9:
+            if int(dateChecker[0]) > 30 or int(dateChecker[0]) < 0:
+                print("Incorrect day's value\n\n")
+                continue
+        if int(dateChecker[1]) == 2:
+            if (int(dateChecker[2]) % 4) == 0:
+                if int(dateChecker[0]) > 29 or int(dateChecker[0]) < 0:
+                    print("Incorrect day's value\n\n")
+                    continue
+            if (int(dateChecker[2]) % 4) == 1:
+                if int(dateChecker[0]) > 28 or int(dateChecker[0]) < 0:
+                    print("Incorrect day's value\n\n")
+                    continue
         file = open(r"D:/YSTCounter/YSTCounter_data.txt", "a")
         file.write("-----------------------\n")
         file.write("Date: %s\n\n" %date)
