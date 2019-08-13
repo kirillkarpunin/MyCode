87fd82a7f65e02826e5a4ba8e3c78d057c73745b
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2019-08-13 16:35:55 +0300

    Change a date input

--- YourSpentTimeCounter.py
+++ YourSpentTimeCounter.py
@@ -37,58 +37,63 @@ while True:
         if not dirChecker:
             print("File not founded\n\n")
             continue
-        date = input("\n\nWhat date is it today? (dd/mm/yyyy)\n")
-        dateChecker = date.split("/")
-        if len(dateChecker) != 3:
-            print("Incorrect date's value\n\n")
-            continue
-        try:
-            int(dateChecker[0])
-        except ValueError:
-            print("Incorrect day's value\n\n")
-            continue
-        try:
-            int(dateChecker[1])
-        except ValueError:
-            print("Incorrect mounth's value\n\n")
-            continue
-        if int(dateChecker[1]) > 12 or int(dateChecker[1]) < 0:
-            print("Incorrect mounth's value\n\n")
-            continue
-        if int(dateChecker[1]) == 1 or int(dateChecker[1]) == 3 or int(dateChecker[1]) == 5 or int(dateChecker[1]) == 7 or int(dateChecker[1]) == 8 or int(dateChecker[1]) == 10 or int(dateChecker[1]) == 12:
-            if int(dateChecker[0]) > 31 or int(dateChecker[0]) < 0:
+        print("\n")
+        while True:
+            date = input("What date is it today? (dd/mm/yyyy)\n")
+            dateChecker = date.split("/")
+            if len(dateChecker) != 3:
+                print("Incorrect date's value\n\n")
+                continue
+            try:
+                int(dateChecker[0])
+            except ValueError:
                 print("Incorrect day's value\n\n")
                 continue
-        if int(dateChecker[1]) == 11 or int(dateChecker[1]) == 4 or int(dateChecker[1]) == 6 or int(dateChecker[1]) == 9:
-            if int(dateChecker[0]) > 30 or int(dateChecker[0]) < 0:
-                print("Incorrect day's value\n\n")
+            try:
+                int(dateChecker[1])
+            except ValueError:
+                print("Incorrect mounth's value\n\n")
                 continue
-        if int(dateChecker[1]) == 2:
-            if (int(dateChecker[2]) % 4) == 0:
-                if int(dateChecker[0]) > 29 or int(dateChecker[0]) < 0:
+            day = int(dateChecker[0])
+            mounth = int(dateChecker[1])
+            year = int(dateChecker[2])
+            if mounth > 12 or mounth < 0:
+                print("Incorrect mounth's value\n\n")
+                continue
+            if mounth == 1 or mounth == 3 or mounth == 5 or mounth == 7 or mounth == 8 or mounth == 10 or mounth == 12:
+                if day > 31 or day < 0:
                     print("Incorrect day's value\n\n")
                     continue
-            if (int(dateChecker[2]) % 4) == 1:
-                if int(dateChecker[0]) > 28 or int(dateChecker[0]) < 0:
+            if mounth == 4 or mounth == 6 or mounth == 9 or mounth == 11:
+                if day > 30 or day < 0:
                     print("Incorrect day's value\n\n")
                     continue
+            if mounth == 2:
+                if year % 4 == 0:
+                    if day > 29 or day < 0:
+                        print("Incorrect day's value\n\n")
+                        continue
+                if year % 4 == 1:
+                    if day > 28 or day < 0:
+                        print("Incorrect day's value\n\n")
+                        continue
+            break
         file = open(r"D:/YSTCounter/YSTCounter_data.txt", "a")
         file.write("-----------------------\n")
         file.write("Date: %s\n\n" %date)
         while True:
-            activity = input("\nEnter your activity\nYou can enter 'stop' here to stop entering\n")
+            activity = input("\n\nEnter your activity\nYou can enter 'stop' here to stop entering\n")
             if activity.lower() == "stop":
                 file.close()
                 break
-            time = input("\nHow much time in hours you spent for this activity\n")
+            time = input("\n\nHow much time in hours you spent for this activity\n")
             try:
                 float(time)
             except ValueError:
                 print("It is not a number\n")
                 continue
             file.write("%s: %s hours\n" %(activity, time))
-            print("")
-        print("Your list was successfully edited\n\n")
+        print("\n\nYour list was successfully edited\n\n")
         continue
     elif menu == "read":
         try:
